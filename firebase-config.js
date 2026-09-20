88bc1c05059e6569098d52158fee70ca9cc923e9
tbksesoko <tbksesoko123@gmail.com>
2026-09-20 18:42:06 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Firebaseコンソール(https://console.firebase.google.com/)で
+// プロジェクトを作成し、「プロジェクトの設定」>「マイアプリ」>「ウェブアプリを追加」
+// で表示される設定値をここに貼り付けてください。
+//
+// このファイルは employee/index.html と admin/index.html の両方から
+// 読み込まれます(1か所で管理)。
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAXGPq3og0lOij91oj24Jm9P88UQ3nxauY",
+  authDomain: "hiltonoffreq.firebaseapp.com",
+  projectId: "hiltonoffreq",
+  storageBucket: "hiltonoffreq.firebasestorage.app",
+  messagingSenderId: "73279957205",
+  appId: "1:73279957205:web:f26903aaf61211755e5a5c"
+};
