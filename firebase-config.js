494061008a3811ae37e651299c3c302c95ee24d5
tbksesoko <tbksesoko123@gmail.com>
2026-09-21 19:08:51 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Firebaseコンソール(https://console.firebase.google.com/)で
+// プロジェクトを作成し、「プロジェクトの設定」>「マイアプリ」>「ウェブアプリを追加」
+// で表示される設定値をここに貼り付けてください。
+//
+// このファイルは employee/index.html と admin/index.html の両方から
+// 読み込まれます(1か所で管理)。
+
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
